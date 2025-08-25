effaaf4b2cebcb83b64dc8e0a4fcb0ed2ce6f84d
Juan Fernado Aaron Britto <juanaaronbritto@outlook.com>
2025-08-25 08:57:19 -0500

    Agregar archivo package-lock.json y establecer conexión a la base de datos PostgreSQL

---
+++ server/connection.js
@@ -0,0 +1,25 @@
+// import 'dotenv/config';
+
+// connect database postgresql
+import { Pool } from "pg";
+
+export const connection_db_pg = new Pool({
+  host: "localhost",    
+  user: "vega",        
+  password: "1234",
+  database: "proyecto_integrador",  
+  port: "5432",         
+  max: 10,              
+});
+
+const testConnection = async () => {
+    let connection;
+  try {
+    connection = await connection_db_pg.connect();
+    console.log("✅ Base de datos conectada correctamente");
+  } catch (error) {
+    console.error(`❌ Error al conectar la base de datos: ${error.message}`);
+  } 
+};
+
+testConnection();
